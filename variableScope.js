4c8e9feabc2709d8ebb716cd6753887654a896f7
PraksaMac <PraksaMac@Slobodans-MacBook-Pro.local>
2023-01-11 11:21:13 +0100

    first commit

---
+++ variableScope.js
@@ -0,0 +1,22 @@
+// function loop (){
+//  for (var i = 0; i < 10; i++) {
+//     var i = 5;
+//     }
+// }
+// console.log(loop);
+
+// function loop (){
+//     for (var i = 0; i < 10; i++) {
+//        let i = 5;
+//        }
+//    }
+//    console.log(loop);
+
+let i = 5;
+function loop (){
+    for (var i = 0; i < 10; i++) {
+        console.log (i);
+    }
+   }
+   console.log(loop);
+   
