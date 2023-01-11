3da0563cfa34c48207f1ced90e5c6f7afde3e50f
PraksaMac <PraksaMac@Slobodans-MacBook-Pro.local>
2023-01-11 13:31:41 +0100

    2 commit

---
+++ anonymous.js
@@ -0,0 +1,12 @@
+const array1 = [2,3,4,5];
+const square= array1.map(x=> x*x );
+console.log(square);
+
+
+var a = 10;
+var b = 20;
+(() => {return a + b})(a ,b)
+
+const timesTwo = (number) =>{
+    return number*2
+}
